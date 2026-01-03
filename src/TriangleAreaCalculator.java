a155541ba6faa4e2e6946d45a72caeaf9b2121e5
Eljen Mae Augusto <86765650+EljenMae@users.noreply.github.com>
2026-01-03 11:33:53 +0800

    Initial commit

--- src/TriangleAreaCalculator.java
+++ src/TriangleAreaCalculator.java
@@ -1,2 +1,26 @@
+/*
+This is the basic of the calculation of the getting the area of the triangle.
+ */
+
+
+import java.util.Scanner;
+
 public class TriangleAreaCalculator {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("--Welcome to Basic Triangle Area Caculator--");
+        System.out.print("Enter width: ");
+        double width = scanner.nextDouble();
+        System.out.print("Enter height: ");
+        double height = scanner.nextDouble();
+
+        //Caculating the area
+        double area = width * height * 0.5;
+
+        //Showing the result
+        System.out.println("The are of the triangle is " + area);
+
+        scanner.close();
+    }
 }
